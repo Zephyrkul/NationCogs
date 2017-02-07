cd6a919012669038818d01d4a7b087260b00979f
zephyrkul <eryk.demarco@gmail.com>
2017-02-06 22:38:43 -0700

    fileIO deprecated, using dataIO

--- skyrim/skyrim.py
+++ skyrim/skyrim.py
@@ -1,16 +1,15 @@
 import discord
 from discord.ext import commands
-from .utils.dataIO import fileIO
+from .utils.dataIO import dataIO
 from random import choice
 import os
 
-
 class GuardLines:
-    """Edited from Airenkun's Insult Cog from 26-Cogs"""
+    """Adapted from Airenkun's Insult Cog from 26-Cogs"""
 
     def __init__(self, bot):
         self.bot = bot
-        self.lines = fileIO("data/skyrim/lines.json", "load")
+        self.lines = dataIO.load_json("data/skyrim/lines.json")
 
     @commands.command()
     async def guard(self):
@@ -18,23 +17,14 @@ class GuardLines:
         await self.bot.say(choice(self.lines))
 
 def check_folders():
-    folders = ("data", "data/skyrim/")
-    for folder in folders:
-        if not os.path.exists(folder):
-            print("Creating " + folder + " folder...")
-            os.makedirs(folder)
+    if not os.path.exists("data/skyrim/"):
+        print("Creating data/skyrim/ folder...")
+        os.makedirs("data/skyrim/")
 
 def check_files():
-    """Moves the file from cogs to the data directory. Important -> Also changes the name to lines.json"""
-    lines = {"I used to be an adventurer like you. Then I took an arrow in the knee..."}
-
+    """Makes sure the cog data exists"""
     if not os.path.isfile("data/skyrim/lines.json"):
-        if os.path.isfile("cogs/put_in_cogs_folder.json"):
-            print("moving default lines.json...")
-            os.rename("cogs/put_in_cogs_folder.json", "data/skyrim/lines.json")
-        else:
-            print("creating default lines.json...")
-            fileIO("data/skyrim/lines.json", "save", lines)
+        raise RuntimeError("Required data is missing. Please reinstall this cog.")
 
 def setup(bot):
     check_folders()
