95142dbce6234c79bf1a0ba7cc0b51707dd5082c
zephyrkul <eryk.demarco@gmail.com>
2017-02-06 19:14:12 -0700

    move [p]agent to [p]shard agent

--- nationshards/nationshards.py
+++ nationshards/nationshards.py
@@ -15,29 +15,29 @@ class NationShards:
 
     def __init__(self, bot):
         self.bot = bot
-        self.settings = dataIO.load_json('data/nationshards/settings.json')
+        self.settings = dataIO.load_json('data/nationstates/settings.json')
         self.api = ns.Api(self.settings['AGENT'])
-
-    @commands.command()
-    @checks.is_owner()
-    async def agent(self, *, agent : str):
-        """Sets the user agent for use with the NationStates API
-
-        Use an informative agent, like an email address. Contact the cog creator (and disable this cog) if you get any threatening emails."""
-        self.settings['AGENT'] = agent
-        dataIO.save_json('data/nationstates/settings.json', self.settings)
-        self.api.user_agent = self.settings['AGENT']
 
     @commands.group(aliases=['shards'], pass_context=True)
     async def shard(self, ctx):
         """Retrieves the specified info from NationStates
 
         Note that some shards provide a lot of data at once; have [p]restart prepared just in case."""
-        if self.api.user_agent is None:
-            await self.bot.say('User agent is not yet set! Set it with `[p]agent` first.')
-            return
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
+
+    @shard.command(name='agent', aliases=['a'], pass_context=True)
+    @checks.is_owner()
+    async def _shard_agent(self, ctx, *, agent : str = None):
+        """Gets or sets the user agent for use with the NationStates API
+
+        Use an informative agent, like an email address. Contact the cog creator (and disable this cog) if you get any threatening emails."""
+        if agent is None:
+            await self.bot.say(box(self.api.user_agent, lang='diff'))
+            await send_cmd_help(ctx)
+        self.settings['AGENT'] = agent
+        dataIO.save_json('data/nationstates/settings.json', self.settings)
+        self.api.user_agent = self.settings['AGENT']
 
     @shard.command(name='nation', aliases=['n'], pass_context=True)
     async def _shard_nation(self, ctx, nation, *shards):
@@ -48,6 +48,9 @@ class NationShards:
         name fullname type category wa gavote scvote freedom region population tax animal animaltrait currency flag banner banners majorindustry crime sensibilities govtpriority govt govdesc industrydesc notable admirable founded firstlogin lastlogin lastactivity influence freedomscores publicsector deaths leader capital religion customleader customcapital customreligion rcensus wcensus censusscore censusscore-N legislation happenings demonym demonym2 demonym2plural factbook factbooklist dispatches dispatchlist zombies"""
         if len(shards) == 0:
             await send_cmd_help(ctx)
+            return
+        if self.api.user_agent is None:
+            await self.bot.say('User agent is not yet set! Set it with `[p]agent` first.')
             return
         if nation[0] == nation[-1] and nation.startswith('"'):
             nation = nation[1:-1]
@@ -68,6 +71,9 @@ class NationShards:
         if len(shards) == 0:
             await send_cmd_help(ctx)
             return
+        if self.api.user_agent is None:
+            await self.bot.say('User agent is not yet set! Set it with `[p]agent` first.')
+            return
         if region[0] == region[-1] and region.startswith('"'):
             region = region[1:-1]
         try:
@@ -87,6 +93,9 @@ class NationShards:
         if len(shards) == 0:
             await send_cmd_help(ctx)
             return
+        if self.api.user_agent is None:
+            await self.bot.say('User agent is not yet set! Set it with `[p]agent` first.')
+            return
         try:
             data = self.api.get_world(list(shards))
             for page in pagify(self._dict_format('\n',data.collect()), ['\n', '[', ':'], shorten_by=16):
@@ -104,6 +113,9 @@ class NationShards:
         if len(shards) == 0:
             await send_cmd_help(ctx)
             return
+        if self.api.user_agent is None:
+            await self.bot.say('User agent is not yet set! Set it with `[p]agent` first.')
+            return	
         if council != '1' and council != '2':
             raise TypeError('Parameter council must be either 1 (GA) or 2 (SC).')
         try:
@@ -117,12 +129,12 @@ class NationShards:
         return base.join('%s : %s'%(key,'%s >%s'%(base, self._dict_format('%s >'%base,value) if isinstance(value,dict) else value)) for key,value in data.items())
 
 def check_folders():
-    if not os.path.exists('data/nationshards'):
-        print('Creating data/nationshards folder...')
-        os.makedirs('data/nationshards')
+    if not os.path.exists('data/nationstates'):
+        print('Creating data/nationstates folder...')
+        os.makedirs('data/nationstates')
 
 def check_files():
-    f = 'data/nationshards/settings.json'
+    f = 'data/nationstates/settings.json'
     data = {'AGENT' : ''}
     if not dataIO.is_valid_json(f):
         dataIO.save_json(f, data)
