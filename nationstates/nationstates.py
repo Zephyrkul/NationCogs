d6ac5118672fa9f3b000ce61a022500530f7986b
zephyrkul <eryk.demarco@gmail.com>
2017-02-06 18:29:18 -0700

    removed default user agent

--- nationstates/nationstates.py
+++ nationstates/nationstates.py
@@ -3,7 +3,10 @@ try:
 except:
     ns = None
 import discord
+import os
 from discord.ext import commands
+from cogs.utils import checks
+from .utils.dataIO import dataIO
 from __main__ import send_cmd_help
 from random import choice
 
@@ -11,11 +14,25 @@ class NationStates:
 
     def __init__(self, bot):
         self.bot = bot
-        self.api = ns.Api('zephyrkul@outlook.com')
+        self.settings = dataIO.load_json('data/nationstates/settings.json')
+        self.api = ns.Api(self.settings['AGENT'])
+
+    @commands.command()
+    @checks.is_owner()
+    async def agent(self, *, agent : str):
+        """Sets the user agent for use with the NationStates API
+
+        Use an informative agent, like an email address. Contact the cog creator (and disable this cog) if you get any threatening emails."""
+        self.settings['AGENT'] = agent
+        dataIO.save_json('data/nationstates/settings.json', self.settings)
+        self.api.user_agent = self.settings['AGENT']
 
     @commands.command()
     async def nation(self, *, nation):
         """Retrieves info about a specified NationStates nation"""
+        if self.api.user_agent is None:
+            await self.bot.say('User agent is not yet set! Set it with `[p]agent` first.')
+            return
         if nation[0] == nation[-1] and nation.startswith('"'):
             nation = nation[1:-1]
         try:
@@ -39,7 +56,7 @@ class NationStates:
         if data['founded'] == '0':
             data['founded'] = 'in Antiquity'
         embed = discord.Embed(title=data['fullname'], url='https://www.nationstates.net/nation=%s'%data['id'], description='[%s](https://www.nationstates.net/region=%s) %s| %s | Founded %s'%(data['region'],regdata['id'],foudel,data['population'],data['founded']), colour=int(''.join([choice('0123456789ABCDEF') for x in range(6)]), 16))
-        embed.set_author(name="NationStates", url='https://www.nationstates.net/')
+        embed.set_author(name='NationStates', url='https://www.nationstates.net/')
         embed.set_thumbnail(url=data['flag'])
         embed.add_field(name=data['category'], value='%s | %s | %s\n\n*"%s"*\n\n%s | %s | %u influence (%s)'%(data['freedom']['civilrights'],data['freedom']['economy'],data['freedom']['politicalfreedom'],data['motto'],data['unstatus'],endo,float(data['census']['scale'][0]['score']),data['influence']), inline=False)
         try:
@@ -50,6 +67,9 @@ class NationStates:
     @commands.command()
     async def region(self, *, region):
         """Retrieves info about a specified NationStates region"""
+        if self.api.user_agent is None:
+            await self.bot.say('User agent is not yet set! Set it with `[p]agent` first.')
+            return
         if region[0] == region[-1] and region.startswith('"'):
             region = region[1:-1]
         try:
@@ -81,7 +101,7 @@ class NationStates:
             except ns.NScore.exceptions.NotFound:
                 data['founder'] += ' (Ceased to Exist)'
         embed = discord.Embed(title=data['name'], url='https://www.nationstates.net/region=%s'%(data['id']), description='[%s nations](https://www.nationstates.net/region=%s/page=list_nations) | Founded %s | Power: %s'%(data['numnations'],data['id'],data['founded'],data['power']), colour=int(''.join([choice('0123456789ABCDEF') for x in range(6)]), 16))
-        embed.set_author(name="NationStates", url='https://www.nationstates.net/')
+        embed.set_author(name='NationStates', url='https://www.nationstates.net/')
         if data['flag']:
             embed.set_thumbnail(url=data['flag'])
         embed.add_field(name='Founder', value=data['founder'], inline=False)
@@ -91,7 +111,20 @@ class NationStates:
         except discord.HTTPException:
             await self.bot.say('I need the `Embed links` permission to send this')
 
+def check_folders():
+    if not os.path.exists('data/nationstates'):
+        print('Creating data/nationstates folder...')
+        os.makedirs('data/nationstates')
+
+def check_files():
+    f = 'data/nationstates/settings.json'
+    data = {'AGENT' : ''}
+    if not dataIO.is_valid_json(f):
+        dataIO.save_json(f, data)
+
 def setup(bot):
     if ns is None:
         raise RuntimeError('You\'re missing the NationStates library.\nInstall it with "pip install nationstates" and reload the module.')
+    check_folders()
+    check_files()
     bot.add_cog(NationStates(bot))
