769c914e8957f9d107602a43cc74dd8a17096028
zephyrkul <zephyrkul@outlook.com>
2017-05-01 01:16:06 -0600

    better act logic

--- act/act.py
+++ act/act.py
@@ -1,3 +1,4 @@
+from copy import copy
 import inflect
 import discord
 from discord.ext import commands
@@ -10,27 +11,33 @@ class Act:
 
     @commands.command(pass_context=True)
     async def act(self, ctx, *, user: discord.Member=None):
-        """If a command isn't found, this command is called instead!"""
+        """Acts the specified user."""
         user = user if user else ctx.message.author
         action = ctx.invoked_with
         if not self.engine.singular_noun(action):
-            action = self.engine.plural(action)
+            action = self.engine.plural_noun(action)
         await self.bot.send_message(ctx.message.channel,
                                     "*{} {}*".format(action, user.mention))
 
     async def on_command_error(self, error, ctx):
         """haxx"""
         if not isinstance(error, commands.CommandNotFound) and \
-                not isinstance(error, commands.CheckFailure):
+                (not isinstance(error, commands.CheckFailure) or
+                 ctx.command.callback == self.act.callback):
             return
-        pre = ctx.prefix + ctx.invoked_with
-        user = ctx.message.content[len(pre):].strip()
-        if user:
-            try:
-                user = commands.MemberConverter(ctx, user).convert()
-            except commands.BadArgument:
-                return
-        await ctx.invoke(self.act, user=user)
+        act = copy(self.act)
+        singular = self.engine.singular_noun(ctx.invoked_with)
+        act.name = ctx.invoked_with
+        act.help = (ctx.invoked_with if singular else self.engine.plural_noun(
+            ctx.invoked_with)).title() + self.act.help[4:]
+        # proper event dispatching
+        self.bot.dispatch("command", act, ctx)
+        try:
+            await act.invoke(ctx)
+        except commands.CommandError as e:
+            act.dispatch_error(e, ctx)
+        else:
+            self.bot.dispatch('command_completion', act, ctx)
 
 
 def setup(bot):
