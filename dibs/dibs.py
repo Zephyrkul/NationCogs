6e8ef0a0512db2cc48ef4af621b0192a3580255f
zephyrkul <zephyrkul@users.noreply.github.com>
2018-03-04 11:33:22 -0700

    dibs cog
    spoiler: it's shite

---
+++ dibs/dibs.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+class Dibs:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_member_leave(self, member):
+        dibs = await self.bot.wait_for_message(check=self.check)
+        await self.bot.add_reaction(dibs, "🏚")
+
+    def check(self, message):
+        server = message.server
+        channel = message.channel
+        if not discord.utils.get(server.roles, name="Dibs Commissioner"):
+            return False
+        if channel.overwrites_for(server.default_role).send_messages is False:
+            return False
+        if not channel.permissions_for(server.me).add_reactions:
+            return False
+        return message.content.lower() == "dibs"
+
+def setup(bot):
+    bot.add_cog(Dibs(bot))
