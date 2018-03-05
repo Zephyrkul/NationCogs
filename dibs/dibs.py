2be28eb739b1a01dda84fad8e9e8eb8ba70d4335
zephyrkul <zephyrkul@users.noreply.github.com>
2018-03-05 11:00:09 -0700

    server check
    because it's not like that's important

--- dibs/dibs.py
+++ dibs/dibs.py
@@ -7,21 +7,22 @@ class Dibs:
         self.bot = bot
 
     async def on_member_remove(self, member):
+        def check(message):
+            server = message.server
+            if server != member.server:
+                return False
+            channel = message.channel
+            if not discord.utils.get(server.roles, name="Dibs Commissioner"):
+                return False
+            if channel.overwrites_for(server.default_role).send_messages is False:
+                return False
+            if not channel.permissions_for(server.me).add_reactions:
+                return False
+            return message.content.lower() == "dibs"
         emoji = ("🏡", "🏠", "🏚")[3 * member.server.role_hierarchy.index(member.top_role) // len(member.server.roles)]
-        dibs = await self.bot.wait_for_message(check=self.check)
+        dibs = await self.bot.wait_for_message(check=check)
         if not discord.utils.get(dibs.reactions, emoji=emoji, me=True):
             await self.bot.add_reaction(dibs, emoji)
-
-    def check(self, message):
-        server = message.server
-        channel = message.channel
-        if not discord.utils.get(server.roles, name="Dibs Commissioner"):
-            return False
-        if channel.overwrites_for(server.default_role).send_messages is False:
-            return False
-        if not channel.permissions_for(server.me).add_reactions:
-            return False
-        return message.content.lower() == "dibs"
 
 def setup(bot):
     bot.add_cog(Dibs(bot))
